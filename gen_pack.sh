f4ebe04cef09cc5bc1bc6aaf3cdf2b71e6062acc
Gabriel Wang <gabriel.wang@arm.com>
2023-11-24 15:40:52 +0000

    Update gen_pack.sh

--- gen_pack.sh
+++ gen_pack.sh
@@ -12,7 +12,7 @@ set -o pipefail
 REQUIRED_GEN_PACK_LIB="0.8.4"
 
 # Set default command line arguments
-DEFAULT_ARGS=()
+DEFAULT_ARGS=(-c "v")
 
 # Pack warehouse directory - destination
 # Default: ./output
